38d85ed502bfe5249736545c86b5ebf44827613f
Vladimir Costa Bezerra <vladimir.cbezerra@gmail.com>
2021-02-26 17:37:32 -0300

    Ciriar endereço

---
+++ modelos/Endereco.js
@@ -0,0 +1,9 @@
+class endereco {
+    constructor(rua,numero,bairro,cidade)
+    {
+        this.rua = rua
+        this.numero = numero
+        this.bairro = bairro
+        this.cidade = cidade
+    }
+}
